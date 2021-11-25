6a8bddf6c234200c9e6ffa2d943197d37400ddfc
Arranz Águeda <marranz.agueda@gmail.com>
2021-11-25 18:53:28 +0100

    fix: html accesibility tag

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -92,9 +92,9 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <span className="text-indigo-400 leading-relaxed">
                 marranz.agueda@gmail.com
-              </a>
+              </span>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
