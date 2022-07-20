b92ac39d37f535b34a2949faa8f2d054762e094a
Mario Arranz <marranz.agueda@gmail.com>
2022-07-20 11:45:31 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -11,21 +11,21 @@ export const projects = [
     subtitle: "Angular architect",
     description:
       "Development of technical architecture and components of applications developed with Angular and Typescript. I also have experiencies leading front end teams",
-      years: 4
+      years: "4 years"
   },
   {
     title: "Everis",
     subtitle: "AngularJs developer",
     description:
       "Development of web applications with AngularJS, Typescript, HTML5, CSS3 and Jenkins technologies for continuous integration and using agile methodologies.",
-      years: 2
+      years: "2 years"
   },
   {
     title: "D.S.I. Eritia S.L.",
     subtitle: "Front end developer",
     description:
       "Web application development using HTML5, CSS3 and javascript technologies.",
-      years: 1
+      years: "1 years"
   },
 ];
 
